7a54fb0099baa902a3c3cd706b3ce7c86e1b5b6e
Dustin Quam <quamsta@gmail.com>
2019-08-15 13:47:28 -0500

    re-enabled fulltext search

--- mysite/_config.php
+++ mysite/_config.php
@@ -3,6 +3,9 @@
 use SilverStripe\Security\PasswordValidator;
 use SilverStripe\Security\Member;
 use SilverStripe\Control\Director;
+use SilverStripe\ORM\Search\FulltextSearchable
+
+FulltextSearchable::enable();
 
 // remove PasswordValidator for SilverStripe 5.0
 $validator = PasswordValidator::create();
